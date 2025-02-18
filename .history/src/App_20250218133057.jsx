3e3920e441a6bb6f3e8bcd01bb2197c1fc130ffd
Marco Lee <marcole32k3@gmail.com>
2025-02-18 13:50:09 -0800

    tailwind and starter

---
+++ .history/src/App_20250218133057.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+      <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-2xl">
+        Tailwind is working! 🚀
+      </div>
+    );
+  }
+  
---
+++ .history/src/App_20250218134459.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>
+    </div>
+  );
+}
